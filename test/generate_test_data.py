01b3571bfc6ed6ae3981f85009934b912c04c376
Jack Neilson <jackdneilson@gmail.com>
2018-03-20 16:53:59 +0000

    Added to test generator script, now generates for essex dataset

--- test/generate_test_data.py
+++ test/generate_test_data.py
@@ -2,7 +2,13 @@ import glob
 
 
 def generate_data(root_dir):
-    for location in glob.glob(root_dir + '/**/*.jpg', recursive=True):
+    for location in glob.glob(root_dir + '/lfw/**/*.jpg', recursive=True):
+        file = open(location[:-3] + 'json', 'w')
+        towrite = '{"name":"%s", "image_location":"%s"}' % (location.split('/')[-2], 'static'+location[1:])
+        file.write(towrite)
+        file.close()
+
+    for location in glob.glob(root_dir + '/essex_cswww/**/*.jpg', recursive=True):
         file = open(location[:-3] + 'json', 'w')
         towrite = '{"name":"%s", "image_location":"%s"}' % (location.split('/')[-2], 'static'+location[1:])
         file.write(towrite)

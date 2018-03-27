75d097821f4c6434048dee988e981bc2e3e5212e
Jack Neilson <jackdneilson@gmail.com>
2018-03-27 14:48:58 +0100

    Forgot http:// scheme

--- test/generate_test_data.py
+++ test/generate_test_data.py
@@ -4,13 +4,13 @@ import glob
 def generate_data(root_dir):
     for location in glob.glob(root_dir + '/lfw/**/*.jpg', recursive=True):
         file = open(location[:-3] + 'json', 'w')
-        towrite = '{"name":"%s", "image_location":"%s"}' % (location.split('/')[-2], 'localhost:8081/static'+location[1:])
+        towrite = '{"name":"%s", "image_location":"%s"}' % (location.split('/')[-2], 'http://localhost:8081/static'+location[1:])
         file.write(towrite)
         file.close()
 
     for location in glob.glob(root_dir + '/essex_cswww/**/*.jpg', recursive=True):
         file = open(location[:-3] + 'json', 'w')
-        towrite = '{"name":"%s", "image_location":"%s"}' % (location.split('/')[-2], 'localhost:8081/static'+location[1:])
+        towrite = '{"name":"%s", "image_location":"%s"}' % (location.split('/')[-2], 'http://localhost:8081/static'+location[1:])
         file.write(towrite)
         file.close()
 

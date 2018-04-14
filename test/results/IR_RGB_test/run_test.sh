13df87d1cb4be97d0e863bc3e26f5634deaa33ab
root <root@RG-N519-05.rgu.ac.uk>
2018-04-14 17:10:08 +0100

    Removed IR_RGB test as pointless. Made changes to poster

--- test/results/IR_RGB_test/run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python3 run.py ./test/SCface/SCface_database/surveillance_cameras_IR_cam8/001_cam8.jpg test_IR_RGB --pthreads=2 --cthreads=2 --maxload=200 --output_location=./test/results/IR_RGB_test;
-python3 run.py ./test/SCface/SCface_database/surveillance_cameras_IR_cam8/012_cam8.jpg test_IR_RGB --pthreads=2 --cthreads=2 --maxload=200 --output_location=./test/results/IR_RGB_test;
-python3 run.py ./test/SCface/SCface_database/surveillance_cameras_IR_cam8/018_cam8.jpg test_IR_RGB --pthreads=2 --cthreads=2 --maxload=200 --output_location=./test/results/IR_RGB_test;
-python3 run.py ./test/SCface/SCface_database/surveillance_cameras_IR_cam8/050_cam8.jpg test_IR_RGB --pthreads=2 --cthreads=2 --maxload=200 --output_location=./test/results/IR_RGB_test;
-python3 run.py ./test/SCface/SCface_database/surveillance_cameras_IR_cam8/123_cam8.jpg test_IR_RGB --pthreads=2 --cthreads=2 --maxload=200 --output_location=./test/results/IR_RGB_test;
